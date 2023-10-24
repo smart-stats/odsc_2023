471d7d566d2bd87e88e78969cc2d4b2c2f793c92
Brian Caffo <bcaffo@gmail.com>
2023-10-24 16:13:26 -0400

    Finished second set of slides

--- part2_code.py
+++ part2_code.py
@@ -1,15 +1,26 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import pandas as pd
 import copy
+import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
 import sklearn as skl
 from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+import sys 
 
-sns.set()
-
+## The following reads in the raw data and processes it
+## Stored ast visit1 and visit2_mricloud.csv
+### Repo at https://github.com/MR-Biomarker-Resource/MRICloudPy
+#sys.path.append("/home/jupyter-bcaffo/sandboxes/MRICloudPy/main")
+#import mricloudpy
+#import read
+#example = "/home/jupyter-bcaffo/sandboxes/MRICloudPy/sample_data/"
+#visit1 = mricloudpy.Data("assets/mricloud/visit1/").df
+#visit2 = mricloudpy.Data("assets/mricloud/visit2/").df
+#visit1.to_csv("assets/visit1_mricloud.csv")
+#visit2.to_csv("assets/visit2_mricloud.csv")
 
 ## Create a random intercept vector
 
@@ -30,4 +41,5 @@ def measurement2():
     y = u + e
     subject = np.repeat( np.array(range(n)), np.repeat(m, n))
     sns.scatterplot(x = subject, y = y);
+    
     
